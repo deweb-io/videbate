dc3579e6f16a93a3a2538e9f9e204e6b0d95ada5
Yaniv Yehuda <yaniv@deweb.io>
2022-12-18 16:15:48 +0200

    Import js modules from the Internets!

--- bbs-common.js
+++ bbs-common.js
@@ -1,7 +1,11 @@
 // Common BBS functionality.
 
-// TODO: Make this work :)
-// import * as firebase from 'https://www.gstatic.com/firebasejs/7.22.1/firebase-app.js';
+// Firebase & Firestore imports.
+let initializeApp, getFirestore, getDocs, collection, query, where;
+
+// Import Firebase & Firestore First.
+await importFirebase();
+await importFirestore();
 
 const Firestore = {
     db: false
@@ -12,20 +16,47 @@ export const initialize = async(firebaseConfig) => {
     if(Firestore.db) {
         return console.error('Firebase already initialized');
     }
-    const fb = firebase.initializeApp(firebaseConfig).firebase_;
-    console.info('Firebase initialized', fb);
 
-    // This doesn't quite work right now - need to find the right path in the object.
-    // Firestore.db = fb.firestore();
+    const app = initializeApp(firebaseConfig);
+    Firestore.db = getFirestore(app);
+    console.info('Firebase initialized');
 };
 
+export async function getPosts() {
+    const querySnapshot = await getDocs(collection(Firestore.db , 'Post'));
+    querySnapshot.forEach((doc) => {
+      console.log(`${doc.id} => ${JSON.stringify(doc.data())}`);
+    });
+}
+
 export async function getPostById(id) {
-    const postRef = await Firestore.db.collection('Post').doc(id);
-    const post = await postRef.get();
-    if(!post.exists) {
-        console.log('No such document!');
-    }   else {
-        console.log('Document data:', post.data());
-        return post.data();
+    const queryBuild = query(collection(Firestore.db, 'Post'), where('id', '==', id));
+    const querySnapshot = await getDocs(queryBuild);
+    querySnapshot.forEach((doc) => {
+        console.log(`${doc.id} => ${JSON.stringify(doc.data())}`);
+    });
+}
+
+async function importFirebase() {
+    let firebase;
+    try {
+        firebase = await import('firebase/app');
+    } catch (error) {
+        firebase = await import('https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js');
     }
+    initializeApp = firebase.initializeApp;
+}
+
+async function importFirestore() {
+    let firestore;
+    try {
+        firestore = await import('firebase/firestore');
+    } catch (error) {
+        firestore = await import('https://www.gstatic.com/firebasejs/9.15.0/firebase-firestore.js');
+    }
+    getFirestore = firestore.getFirestore;
+    getDocs = firestore.getDocs;
+    collection = firestore.collection;
+    query = firestore.query;
+    where = firestore.where;
 }
