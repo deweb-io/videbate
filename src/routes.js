4ee97acfed8ee16fd50b8e5a8eace63a154e1db5
Yaniv Yehuda <yaniv@deweb.io>
2023-01-08 12:02:48 +0200

    upload video to gcp storage

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,13 @@
 import * as fs from 'fs';
 
+import { Storage } from '@google-cloud/storage';
+import multer from 'fastify-multer';
+
 import * as db from './db.js';
 
-import util from 'util';
-import { pipeline } from 'stream';
-const pump = util.promisify(pipeline)
+const storage = new Storage({projectId: 'creator-eco-stage'});
+const bucket = storage.bucket(process.env.GCP_BUCKET_NAME);
+const upload = multer({storage: multer.memoryStorage()});
 
 const showTemplate = fs.readFileSync('./showTemplate.html', 'utf8');
 
@@ -26,7 +29,7 @@ export default async(fastify, _) => {
             title: 'Videbate', description: fs.readFileSync('./README.md', 'utf8'), version: '0.1.0'
         }}});
         await fastify.register(await import('@fastify/swagger-ui'), {routePrefix: '/doc'});
-        await fastify.register(await import('@fastify/multipart'));
+        await fastify.register(multer.contentParser);
     }
 
     // A health check - let's make it a bit more thorough.
@@ -61,23 +64,36 @@ export default async(fastify, _) => {
         return response.code(404).type('text/plain').send('file not found');
     });
 
-    // return upload video page
+    // Upload video page
     fastify.get('/uploadpage', async(request, response) => {
         const content = fs.readFileSync('./upload.html', 'utf8');
         return response.type('text/html').send(content);
     });
 
-    // upload a video
-    fastify.post('/upload', async(request, response) => {     
-        const data = await request.file();
-        const { filename, mimetype, encoding, file } = data;
-        console.log(filename, mimetype, encoding);
-        console.log(file);
-
-        // save file to uploads directory 
-        const saveTo = `./uploads/${filename}`;
-        pump(file, fs.createWriteStream(saveTo));
-
-        return response.code(201).send({filename, mimetype, encoding});
-    });
+    // Upload a video
+    fastify.post('/upload', {preHandler: upload.single('video'), }, async(req, res) => {     
+        const file = req.file;
+        if (!file) {
+            return res.status(400).send('No file was uploaded.');
+        }        
+      
+        const gcsFile = bucket.file(`videbate/${file.originalname}`);
+        const stream = gcsFile.createWriteStream({
+          metadata: {
+            contentType: file.mimetype,
+          },
+        });
+      
+        stream.on('error', (err) => {
+            console.error(err);
+            return res.status(500).send(err);
+        });
+      
+        stream.on('finish', () => {
+            // reponse was already sent by the multer middleware
+            console.log(`File ${file.originalname} was uploaded to GCS.`);
+        });
+      
+        stream.end(file.buffer);
+      });
 };
